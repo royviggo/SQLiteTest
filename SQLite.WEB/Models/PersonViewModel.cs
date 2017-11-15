e895ca18961c069c3e1bfffbe410bb9c623aca46
royviggo <royviggo@gmail.com>
2017-11-16 00:36:19 +0100

    Added created and modified date to Person view model. Changes to Person details layout.

--- SQLite.WEB/Models/PersonViewModel.cs
+++ SQLite.WEB/Models/PersonViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace SQLite.WEB.Models
+﻿using System;
+
+namespace SQLite.WEB.Models
 {
     public class PersonViewModel
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
     }
 }
