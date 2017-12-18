95c5a97f44ffd6bf6188dafcd5d7aac58f29ab95
royviggo <royviggo@gmail.com>
2017-12-18 09:01:51 +0100

    Changed attribute names

--- SQLite.DAL/DomainModels/Person.cs
+++ SQLite.DAL/DomainModels/Person.cs
@@ -9,37 +9,31 @@ namespace SQLite.DAL.DomainModels
 {
     public class Person : IEntity
     {
-        [Key, Column("id")]
+        [Key]
         public int Id { get; set; }
 
-        [Column("first_name"), MaxLength(255)]
+        [MaxLength(255)]
         public string FirstName { get; set; }
 
-        [Column("father_name"), MaxLength(255)]
+        [MaxLength(255)]
         public string FatherName { get; set; }
 
-        [Column("patronym"), MaxLength(255)]
+        [MaxLength(255)]
         public string Patronym { get; set; }
 
-        [Column("last_name"), MaxLength(255)]
+        [MaxLength(255)]
         public string LastName { get; set; }
 
-        [Column("gender")]
         public Gender Gender { get; set; }
 
-        [Column("born_year")]
         public int? BornYear { get; set; }
 
-        [Column("death_year")]
         public int? DeathYear { get; set; }
 
-        [Column("status")]
         public Status Status { get; set; }
 
-        [Column("created_date")]
         public DateTime? CreatedDate { get; set; }
 
-        [Column("modified_date")]
         public DateTime? ModifiedDate { get; set; }
 
         [ForeignKey("PersonId")]
